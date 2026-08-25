727cb5b181ba0dab68e2a713a5e5fb65bed0fdc9
Gyunhee Lee <92011224+GyunHeee@users.noreply.github.com>
2026-08-25 22:24:15 +0900

    [level 1] Title: 최소직사각형, Time: 0.76 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,30 +1,29 @@
 class Solution {
     public int solution(int[][] sizes) {
         
-        int maxWeight = 0;
-        int maxHeight = 0;
+        int maxW = 0;
+        int maxH = 0;
         
-        for (int i=0; i<sizes.length;i++) {
-            if (sizes[i][0] < sizes[i][1]) {
-                int temp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = temp;
+        for (int[] size: sizes) {
+            int a = size[0];
+            int b = size[1];
+            
+            if (a < b) {
+                size[1] = a;
+                size[0] = b; 
             }
         }
         
-        for (int i=0; i<sizes.length;i++) {
-            if (sizes[i][0] > maxWeight) {
-                maxWeight= sizes[i][0];
-                maxHeight = sizes[i][1];
+        for (int[] size: sizes) {
+            if (size[0] > maxW) {
+                maxW = size[0];
+            }
+            
+            if (size[1] > maxH) {
+                maxH = size[1];
             }
         }
         
-        for (int i=0; i<sizes.length;i++) {
-            if (sizes[i][1] > maxHeight) {
-                maxHeight = sizes[i][1];
-            }
-        }
-        
-        return maxWeight * maxHeight;
+        return maxW * maxH;
     }
 }
