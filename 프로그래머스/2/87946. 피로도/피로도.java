8968ea9e4a53454555ccf5ad881d62aad284ce07
Gyunhee Lee <92011224+GyunHeee@users.noreply.github.com>
2026-09-08 14:31:30 +0900

    [level 2] Title: 피로도, Time: 1.61 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,27 +1,27 @@
 import java.util.*;
 
 class Solution {
-    int answer = -1;
-    boolean[] visited;
+    // 탐색문제 8!이 최대경우
+    
+    int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        
-        dfs(k, dungeons, 0);
-
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(k, dungeons, visited, 0);
         return answer;
     }
     
-    void dfs(int k, int[][] dungeons, int count) {
-        
-        answer = Math.max(count, answer);
+    void dfs(int k, int[][] dungeons, boolean[] visited, int count) {
+        answer = Math.max(answer, count);
         
         for (int i=0; i<dungeons.length; i++) {
-            if (dungeons[i][0] <= k && !visited[i]) {
-                visited[i] = true;
-                dfs(k - dungeons[i][1], dungeons, count + 1);
-                visited[i] = false;
-            }
+            if (dungeons[i][0] > k) continue;
+            
+            if (visited[i]) continue;
+            
+            visited[i] = true;
+            dfs(k - dungeons[i][1], dungeons, visited, count + 1);
+            visited[i] = false;
         }
     }
 }
