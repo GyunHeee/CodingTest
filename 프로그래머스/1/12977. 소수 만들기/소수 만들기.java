48af96488a773e0257d9be99cd2d42dee64ab422
Gyunhee Lee <92011224+GyunHeee@users.noreply.github.com>
2026-09-10 16:50:00 +0900

    [level 1] Title: 소수 만들기, Time: 278.48 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,44 +1,10 @@
 class Solution {
-    
-    int answer = 0;
-    
     public int solution(int[] nums) {
-        boolean[] visited = new boolean[nums.length];
-        dfs(0, nums, visited, 0);
+        int answer = -1;
 
-        return answer / 6;
-    }
-    
-    boolean isPrime(int num) {
-        boolean prime = true;
-        
-        if (num == 1 || num == 0) {
-            prime = false;
-        }
-        
-        for (int i=2; i<num; i++) {
-            if (num % i == 0) prime = false;
-        }
-        
-        return prime;
-    }
-    
-    void dfs(int sum, int[] nums, boolean[] visited, int count) {
-        
-        if (count == 3) {
-            if (isPrime(sum)) {
-                answer++;
-            }
-            
-            return;
-        }
-        
-        for (int i=0; i<nums.length; i++) {
-            if (visited[i]) continue;
-            
-            visited[i] = true;
-            dfs(sum + nums[i], nums, visited, count + 1);
-            visited[i] = false;
-        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
     }
 }
