8867c2d7f71a3558e5925856d7a73e600998ecca
Gyunhee Lee <92011224+GyunHeee@users.noreply.github.com>
2026-09-15 18:10:02 +0900

    [level 3] Title: 가장 먼 노드, Time: 17.22 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,42 +3,46 @@ import java.util.*;
 class Solution {    
     public int solution(int n, int[][] edge) {
         int answer = 0;
+        
         List<List<Integer>> graph = new ArrayList<>();
         Queue<Integer> queue = new ArrayDeque<>();
-        int[] degrees = new int[edge.length + 1];
+        int[] degrees = new int[n + 1];
         
-        for (int i=0; i<=n; i++) {
+        for (int i=0; i<= n ;i++) {
             graph.add(new ArrayList<>());
         }
         
-        for (int[] e : edge) {
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
+        for (int[] e: edge) {
+            int a = e[0];
+            int b = e[1];
+            
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
         
-        
         queue.offer(1);
-        degrees[1] = 0;
-        int currentDegree = 0;
         
         while (!queue.isEmpty()) {
-            int node = queue.poll();
+            int cur = queue.poll();
             
-            for (int g : graph.get(node)) {
-                if (degrees[g] == 0) {
-                    degrees[g] = degrees[node] + 1;
-                    queue.offer(g);
+            for (int next: graph.get(cur)) {
+                
+                if (degrees[next] == 0) {
+                    degrees[next] = degrees[cur] + 1;
+                    queue.offer(next);
                 }
             }
         }
-        
+
         int max = 0;
-        for (int i=2; i<degrees.length; i++) {
-            max = Math.max(max, degrees[i]);
+        for (int i=2; i<=n;i++) {
+            max = Math.max(degrees[i], max);
         }
         
-        for (int i=2; i<degrees.length; i++) {
-            if (max == degrees[i]) answer++;
+        for (int i=2; i<=n; i++) {
+            if (max == degrees[i]) {
+                answer++;
+            }
         }
         
         return answer;
